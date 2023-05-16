12c9dbc3ec0d677604bc3bbc32b4e59218c178a7
Markus Rathgeb <maggu2810@gmail.com>
2023-05-16 23:34:48 +0200

    begin to support podman and docker (correct arch required)
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- fedora/image-build.sh
+++ fedora/image-build.sh
@@ -2,4 +2,14 @@
 
 . my-env
 
-podman image build --arch "${MY_ARCH}" -t "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}" -f Containerfile
+case "${MY_CME}" in
+  podman)
+    podman image build --arch "${MY_ARCH}" -t "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}" -f Containerfile
+    ;;
+  docker)
+    docker image build -t "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}" -f Containerfile .
+    ;;
+  *)
+    echo "unknown container management engine" >&2
+    exit 1
+esac
--- fedora/image-push.sh
+++ fedora/image-push.sh
@@ -2,4 +2,14 @@
 
 . my-env
 
-podman image push "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}"
+case "${MY_CME}" in
+  podman)
+    podman image push "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}"
+    ;;
+  docker)
+    docker image push "${MY_REGISTRY}/${MY_IMAGE}:${MY_TAG}"
+    ;;
+  *)
+    echo "unknown container management engine" >&2
+    exit 1
+esac
